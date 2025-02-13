f246d55e7f6332cc83c792d962d5eb876cb54d5a
Lance Hill <lance@prometheussoftware.ca>
2025-02-13 15:09:13 -0500

    Watch for changes in the project-name-activity channel. The channel should be reporting github activity

--- src/main/kotlin/tech/penser/MyRoutes.kt
+++ src/main/kotlin/tech/penser/MyRoutes.kt
@@ -23,12 +23,25 @@ class MyRoutes : RouteBuilder() {
         from("slack://general?token=RAW({{env:SLACK_CAMEL_BOT_TOKEN}})")
             .id("slack-bot")
             .process { exchange ->
-                val payload = exchange.getMessage().getBody(com.slack.api.model.Message::class.java)
+Watch                val payload = exchange.message.getBody(com.slack.api.model.Message::class.java)
                 exchange.message.setHeader("userId", payload.user)
             }
             // filter out messages coming from the bot
             .filter { exchange -> exchange.message.getHeader("userId") != "U078BNJH8BH" }
             .log("\${body}")
             .to("slack://general?token=RAW({{env:SLACK_CAMEL_BOT_TOKEN}})")
+
+        from("slack://nosy-narwhal-activity?token=RAW({{env:SLACK_CAMEL_BOT_TOKEN}})")
+            .id("narwhal-bot")
+            .process { exchange ->
+                val payload = exchange.message.getBody(com.slack.api.model.Message::class.java)
+                exchange.message.setHeader("userId", payload.user)
+            }
+            // filter out messages coming from the bot
+            .filter { exchange -> exchange.message.getHeader("userId") != "U078BNJH8BH" }
+            // need to filter out or alter messages so they do not trigger the bot to issue
+            // commands simply by repeating what it heard
+            .log("\${body}")
+//            .to("slack://general?token=RAW({{env:SLACK_CAMEL_BOT_TOKEN}})")
     }
 }
